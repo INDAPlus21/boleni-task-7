526d5c72f0e9727ac0d42cdedf5c9e1241e065ee
djonte <90456387+djonte@users.noreply.github.com>
2021-11-05 15:34:29 +0100

    unsigned long int fix

--- cyber_clara/cyber_klara.c
+++ cyber_clara/cyber_klara.c
@@ -3,40 +3,38 @@
 #include <stdlib.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    unsigned long int n;
+    scanf("%lu", &n);
 
     char names[2*n][21];
 
-    for (int i = 0; i < n; i++) {
+    for (unsigned long int i = 0; i < n; i++) {
         scanf("%s", names[i]);
     }
 
-    for (int i = 0; i < n; i++) {
+    for (unsigned long int i = 0; i < n; i++) {
         scanf("%s", names[n+i]);
     }
 
     char allNames[n][42];
-    for (int i = 0; i < n; i++) {
+    for (unsigned long int i = 0; i < n; i++) {
         char target[42];
         strcpy(target, names[i]);
+        strcat(target, "_");
         strcat(target, names[n+i]);
         strcpy(allNames[i], target);
     }
 
     qsort(allNames, n, 42, strcmp);
 
-    for(int i = 0; i < n; i++) {
-        printf("%s\n", allNames[i]);
-    }
+    unsigned long int doublis = 0;
 
-    int doublis = 0;
-    for (int i = 0; i < n; i++) {
-        if (strcmp(allNames[i], allNames[i+1]) == 0) {
+    for (unsigned long int i = 0; i < n; i++) {
+        if (strcmp(allNames[i], allNames[i+1]) == 0 && i != n-1) {
             doublis++;
         }
     }
 
-    printf("%d", n-doublis);
+    printf("%lu", n-doublis);
     return 0;
 }
